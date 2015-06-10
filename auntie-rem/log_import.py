b7bb813080b45774a1b9331cfdab764cfc0eb17d
wvxvw <olegsivokon@gmail.com>
2015-06-10 20:36:13 +0300

    Building index and conversations as we import the data.

--- auntie-rem/log_import.py
+++ auntie-rem/log_import.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import re
+import os
 import requests
 import logging
 
+from string import punctuation
 from datetime import datetime
-from sqlalchemy import create_engine, Column, Integer, String, Sequence, ForeignKey, DateTime, and_
+from sqlalchemy import create_engine, Table, Column, Integer, String, \
+    Sequence, ForeignKey, DateTime, and_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, backref
 from lxml.html import parse as html_parse
@@ -42,6 +45,7 @@ class Message(Base):
     command = Column(String)
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship('User', backref = backref('messages', order_by = id))
+    conversation_id = Column(Integer, ForeignKey('conversations.id'))
     
     def __repr__(self):
         if self.command:
@@ -56,44 +60,183 @@ Message.cause_id = Column(Integer, ForeignKey(Message.id))
 Message.in_response_to = relationship(Message, backref = 'messages',
     remote_side = Message.id)
 
+word_index = Table('word_association', Base.metadata,
+    Column('index_id', Integer, ForeignKey('index.id')),
+    Column('conversation_id', Integer, ForeignKey('conversations.id')))
 
+class Conversation(Base):
+    __tablename__ = 'conversations'
+    id = Column(Integer, Sequence('conversation_id_seq'), primary_key = True)
+    messages = relationship('Message', order_by = 'Message.id',
+                            backref = 'conversations')
 
-def find_or_create_user(nick):
-    user = session.query(User).filter_by(nick = nick).first()
-    if not user:
-        user = User()
-        user.nick = nick
-    return user
+class Index(Base):
+    __tablename__ = 'index'
+    id = Column(Integer, Sequence('word_id_seq'), primary_key = True)
+    word = Column(String, nullable = False)
+    conversations = relationship('Conversation',
+                                 order_by = 'Conversation.id',
+                                 secondary = word_index)
 
-def parse_message(date, line):
-    # 00:00:00 --- log: started lisp/15.01.01
-    # 00:06:42 <pjb> Cons Ignucius.
-    result = Message()
-    cmd = r'((\d\d):(\d\d):(\d\d)) --- ([^:]+): (.*)'
-    msg = r'((\d\d):(\d\d):(\d\d)) <([^)]+)> (.*)'
-    match = re.match(cmd, line)
-    if not match: match = re.match(msg, line)
-    if match:
-        result.ts = datetime(*(date + [int(x) for x in match.group(2, 3, 4)]))
-        result.user = find_or_create_user(match.group(5))
-        result.text = match.group(6)
-    return result
+class MessageParser(object):
+    stopwords = None
+    script_home = os.path.realpath(os.path.dirname(__file__))
 
-def clean_lines(content):
-    line, begin, end = None, 0, 0
-    for byte in content:
-        end += 1
-        if byte == '\n':
+    def __init__(self, state):
+        self.state = state
+        self.splitter = re.compile(r'[\s{}]+'.format(re.escape(punctuation)))
+        self.cmd = re.compile(r'((\d\d):(\d\d):(\d\d)) --- (join|quit): ([^ ]+) (.*)')
+        self.msg = re.compile(r'((\d\d):(\d\d):(\d\d))( )<([^)>]+)> (.*)')
+
+        if not MessageParser.stopwords:
+            with open(os.path.join(
+                    MessageParser.script_home, 'stopwords.txt'), 'r') as f:
+                MessageParser.stopwords = {(x, True) for x in f}
+    
+    def parse_message(self, date, line):
+        # 00:00:00 --- log: started lisp/15.01.01
+        # 00:06:42 <pjb> Cons Ignucius.
+        result = Message()
+        match = re.match(self.cmd, line)
+        is_message = not match
+
+        if is_message: match = re.match(self.msg, line)
+        if match:
+            result.ts = datetime(*(date + [int(x) for x in match.group(2, 3, 4)]))
+            result.user = self.state.find_or_create_user(match.group(6))
             try:
-                line = content[begin : end].decode('ascii')
-            except UnicodeDecodeError:
+                result.text = match.group(7)
+            except:
+                result.text = ''
+            logging.debug('Added message: "%s"' % result.text)
+        if result.text:
+            self.state.messages.append(result)
+        if not is_message:
+            if match.group(5) == 'quit':
+                self.state.forget_user(result.user)
+        if is_message and result.text:
+            for unanswered in self.state.unanswered_messages:
+                if result.user.nick in unanswered.text:
+                    result.in_response_to = unanswered
+                    self.state.unanswered_messages.remove(unanswered)
+                    break
+            convo = self.state.conversation_for_message(result)
+            if not convo:
+                convo = self.state.open_conversation(result)
+            else: self.state.say(result, convo)
+            self.index_words(result, convo)
+
+    def index_words(self, message, convo):
+        for word in re.split(self.splitter, message.text):
+            if not word in MessageParser.stopwords:
+                index = self.state.index.get(word, None)
+                if not index: index = Index()
+                index.word = word
+                convos = index.conversations or []
+                convos.append(convo)
+                index.conversations = convos
+                self.state.index[word] = index
+
+    def clean_lines(self, content):
+        line, begin, end = None, 0, 0
+        for byte in content:
+            end += 1
+            if byte == '\n':
                 try:
-                    line = content[begin : end].decode('utf-8')
+                    line = content[begin : end].decode('ascii')
                 except UnicodeDecodeError:
-                    begin = end
-                    continue
-            begin = end
-            yield line
+                    try:
+                        line = content[begin : end].decode('utf-8')
+                    except UnicodeDecodeError:
+                        begin = end
+                        continue
+                begin = end
+                yield line
+
+    def parse(self, archives, options):
+        done = 0
+        for arch in archives:
+            if re.match(r'\d\d\.\d\d\.\d\d', arch):
+                if options.max > -1 and done >= options.max:
+                    break
+                logging.info('%d of %d importing http://tunes.org/~nef/logs/lisp/%s' % \
+                             (done, options.max, arch))
+                log_file = requests.get('http://tunes.org/~nef/logs/lisp/%s' % arch)
+                date = [int(x) for x in arch.split('.')]
+                for line in self.clean_lines(log_file.content):
+                    self.parse_message(date, line)
+                done += 1
+
+class ChatState(object):
+
+    def __init__(self):
+        self.users_online = set()
+        self.active_conversations = []
+        self.index = {}
+        self.messages = []
+        self.parser = MessageParser(self)
+        self.unanswered_messages = []
+
+    def conversation_for_message(self, message):
+        # TODO: This needs improvement to recognize conversations.
+        for convo in self.active_conversations:
+            for old_message in convo.messages:
+                if message.in_response_to == old_message \
+                   or old_message.user == message.user \
+                   or (message.user and message.user.nick in old_message.text):
+                    return convo
+        return None
+        
+    def open_conversation(self, message):
+        convo = Conversation()
+        convo.messages = [message]
+        self.unanswered_messages.append(message)
+        self.users_online.add(message.user)
+        self.active_conversations.append(convo)
+        return convo
+
+    def close_conversation(self, convo):
+        self.active_conversations.remove(convo)
+        session.add(convo)
+        for message in convo.messages:
+            try:
+                self.unanswered_messages.remove(message)
+            except:
+                pass
+
+    def forget_user(self, user):
+        try:
+            self.users_online.remove(user)
+        except:
+            pass
+        for convo in self.active_conversations:
+            should_remove = True
+            for message in convo.messages:
+                if message.user in self.users_online:
+                    should_remove = False
+                    break
+            if should_remove:
+                self.close_conversation(convo)
+
+    def say(self, message, convo):
+        self.users_online.add(message.user)
+        self.unanswered_messages.append(message)
+        convo.messages.append(message)
+
+    def word_index(self):
+        return self.index.values()
+
+    def find_or_create_user(self, nick):
+        user = session.query(User).filter_by(nick = nick).first()
+        if not user:
+            user = User()
+            logging.info('Created user: %s' % nick)
+            user.nick = nick
+            session.add(user)
+        return user
+
+    def objects(self):
+        return self.messages + self.active_conversations + self.word_index()
 
 def start_engine(options):
     global session
@@ -108,16 +251,10 @@ def start_engine(options):
 
 def populate(options):
     start_engine(options)
-    logs = html_parse(options.url)
-    archives = logs.xpath('//td/a/text()')
-    for arch in archives:
-        if re.match(r'\d\d\.\d\d\.\d\d', arch):
-            
-            print 'will import http://tunes.org/~nef/logs/lisp/%s' % arch
-            log_file = requests.get('http://tunes.org/~nef/logs/lisp/%s' % arch)
-            date = [int(x) for x in arch.split('.')]
-            session.add_all(parse_message(date, line)
-                            for line in clean_lines(log_file.content))
+    archives = html_parse(options.url).xpath('//td/a/text()')
+    state = ChatState()
+    state.parser.parse(archives, options)
+    session.add_all(state.objects())
     session.commit()
                 
 def test(options):
@@ -131,8 +268,8 @@ if __name__ == '__main__':
         if not value in ['swf', 'swc']:
             parser.error('Supported formats are swf and swc.')
     group = OptionGroup(parser, 'Database options',
-                        'These optins manage connection to the database.')
-    group.add_option('-p', '--port', dest = 'port', default = 5432,
+                        'These options manage connection to the database.')
+    group.add_option('-p', '--port', dest = 'port', default = 5432, type = 'int',
                      help = 'Port where Postgresql server listens.')
     group.add_option('-u', '--user', dest = 'user', default = 'auntie',
                      help = 'Postgresql user.')
@@ -143,9 +280,14 @@ if __name__ == '__main__':
     group.add_option('-w', '--password', dest = 'password', default = 'auntie-password',
                      help = 'Postgresql connection password.')
     parser.add_option_group(group)
-    parser.add_option(
+    group = OptionGroup(parser, 'Import options',
+                        'These options manage information import.')
+    group.add_option(
         '-l', '--logs-url', dest = 'url', default = 'http://tunes.org/~nef/logs/lisp/',
         help = 'URL to fetch the logs from (this option is incompatible with --test)')
+    group.add_option(
+        '-m', '--max', dest = 'max', default = -1, type = 'int',
+        help = 'Parse at most this many logs (negative values mean no restriction)')
     parser.add_option(
         '-t', '--test', dest = 'test', action = 'store_true', default = False,
         help = 'Instruct the script to start an interactive session.')
